2ede6b29ad147c4a87370d049e97f4c2e7f51a73
Van Lam <lamvanr@gmail.com>
2023-05-05 13:28:16 +0700

    Add project files.

---
+++ StopWatch/SelectionSort.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    public class Array
+    {
+        private int Arr_Size = 100000;
+        private int[] arr;
+        
+        private string _result;
+        public string Result
+        {
+            get { return _result; }
+            set { _result = value; }
+        }
+        public Array()
+        {
+            StopWatch stopwatch = new StopWatch();
+            stopwatch.Start();
+            arr = new int[this.Arr_Size];
+            Random rand = new Random();
+            for (int i=0; i < this.Arr_Size; i++)
+            {
+                arr[i] = rand.Next();
+            }
+
+
+            for (int i = 0; i < this.Arr_Size -1; i++)
+            {
+                int minIndex = i;
+                for (int j = 1 + 1; j < this.Arr_Size; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                int temp = arr[i];
+                arr[i] = arr[minIndex];
+                arr[minIndex] = temp;
+            }
+            stopwatch.Stop();
+            stopwatch.GetElapsedTime();
+            this.Result = Convert.ToString(stopwatch.GetElapsedTime());
+        }
+
+        public override string ToString()
+        {
+            return $"Elapsed time: {Result}";
+        }
+    }
+}
---
+++ StopWatch/StopWatch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    public class StopWatch
+    {
+        private DateTime startTime;
+        private DateTime stopTime;
+        private bool running = false;
+
+        public void Start()
+        {
+            this.startTime = DateTime.Now;
+            this.running = true;
+        }
+
+        public void Stop()
+        {
+            this.stopTime = DateTime.Now;
+            this.running = false;
+        }
+
+        public TimeSpan GetElapsedTime()
+        {
+            TimeSpan elapsed;
+            if (running)
+                elapsed = DateTime.Now - startTime;
+            else
+                elapsed = stopTime - startTime;
+            return elapsed;
+        }
+
+    }
+}
